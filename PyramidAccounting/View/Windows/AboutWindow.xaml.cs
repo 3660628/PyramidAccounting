1a2fa2b75cb73d15107e2e74c914edf6d0dfdcab
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-10 16:26:58 +0800

    Init Project

--- PyramidAccounting/View/Windows/AboutWindow.xaml.cs
+++ PyramidAccounting/View/Windows/AboutWindow.xaml.cs
@@ -9,17 +9,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PA
+namespace PA.View.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AboutWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AboutWindow : Window
     {
-        public MainWindow()
+        public AboutWindow()
         {
             InitializeComponent();
         }
