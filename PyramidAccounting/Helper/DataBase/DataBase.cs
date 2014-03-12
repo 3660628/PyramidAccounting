baf3a18e1bd4672b6158f99f0a1d2a30b5c0412c
lijianbin <361931469@qq.com>
2014-03-12 15:22:37 +0800

    数据库操作类新增

--- PyramidAccounting/Helper/DataBase/DataBase.cs
+++ PyramidAccounting/Helper/DataBase/DataBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Data.SQLite;
+using System.Data;
 
 namespace PA.Helper.DataBase
 {
@@ -71,6 +72,95 @@ namespace PA.Helper.DataBase
             {
                 conn.Close();
                 conn.Dispose();
+            }
+            return flag;
+        }
+
+        public string GetAllData(string sql)
+        {
+            SQLiteConnection conn = DBInitialize.getDBConnection();
+            StringBuilder sb = new StringBuilder();
+            string result = "";
+            conn.Open();
+            try
+            {
+                SQLiteCommand cmd = new SQLiteCommand();
+                cmd.CommandText = sql;
+                cmd.Connection = conn;
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            string dbType = reader.GetDataTypeName(i);
+                            switch (dbType)
+                            {
+                                case "DATE":
+                                    sb = sb.Append(reader.GetDateTime(i).ToString("yyyy-MM-dd")).Append(",");
+                                    break;
+                                default:
+                                    sb = sb.Append(reader.GetValue(i)).Append(",");
+                                    break;
+                            }
+                        }
+                        sb.Append("\t");
+                    }
+                    result = sb.ToString().ToString();
+                }
+            }
+            catch (SQLiteException e)
+            {
+                Console.WriteLine("异常:" + e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return result;
+        }
+        /// <summary>
+        /// 根据查询的SQL返回一个dataset
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public DataSet Query(string sql)
+        {
+            DataSet ds = new DataSet();
+            SQLiteConnection conn = DBInitialize.getDBConnection();
+            conn.Open();
+            try
+            {
+                SQLiteDataAdapter my = new SQLiteDataAdapter(sql, conn);
+                my.Fill(ds);
+            }
+            catch (SQLiteException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("SQL{0}:" + sql);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            return ds;
+        }
+
+
+        /// <summary>
+        /// 判断数据是否存在
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public bool IsExist(string sql)
+        {
+            bool flag = false;
+            if (GetAllData(sql).Length > 0)
+            {
+                flag = true;
             }
             return flag;
         }
