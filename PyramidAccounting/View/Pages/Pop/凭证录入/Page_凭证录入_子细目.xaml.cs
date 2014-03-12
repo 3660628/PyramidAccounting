781ea3c81c228ec019f4268bd16ff5f0b8a9ae34
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-12 23:16:24 +0800

    添加科目,子细目下拉菜单的page

---
+++ PyramidAccounting/View/Pages/Pop/凭证录入/Page_凭证录入_子细目.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PA.View.Pages.Pop.凭证录入
+{
+    /// <summary>
+    /// Interaction logic for Page_凭证录入_子细目.xaml
+    /// </summary>
+    public partial class Page_凭证录入_子细目 : Page
+    {
+        public Page_凭证录入_子细目()
+        {
+            InitializeComponent();
+        }
+    }
+}
