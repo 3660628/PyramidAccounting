7db0b804d5ea1803d2bf2dda4341f85b63829f79
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-12 16:55:48 +0800

    阿斯顿

--- PyramidAccounting/View/Windows/Win_凭证输入.xaml.cs
+++ PyramidAccounting/View/Windows/Win_凭证输入.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using PA.Model.DataGrid;
 
 namespace PA.View.Windows
 {
@@ -21,6 +22,13 @@ namespace PA.View.Windows
         public Win_凭证输入()
         {
             InitializeComponent();
+            Model_凭证单 m = new Model_凭证单();
+            m.凭证明细 = new List<Model_凭证明细>();
+            for (int i = 0; i < 6; i++ )
+            {
+                m.凭证明细.Add(new Model_凭证明细());
+            }
+            this.DataGrid_凭证明细.ItemsSource = m.凭证明细;
         }
 
         private void Button_Close_Click(object sender, RoutedEventArgs e)
