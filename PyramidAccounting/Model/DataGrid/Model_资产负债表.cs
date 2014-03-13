d59818631f8eedc59481b6b168320668c1684ae2
lijianbin <361931469@qq.com>
2014-03-13 16:19:37 +0800

    提价

---
+++ PyramidAccounting/Model/DataGrid/Model_资产负债表.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PA.Model.DataGrid
+{
+    class Model_资产负债表
+    {
+        private string col_01;  //科目编号
+        private string col_02;  //资产部类
+        private decimal col_03; //年初数
+        private decimal col_04; //期末数
+        private string col_05;
+        private string col_06;
+        private decimal col_07;
+        private decimal col_08;
+
+        public decimal Col_08
+        {
+            get { return col_08; }
+            set { col_08 = value; }
+        }
+
+        public decimal Col_07
+        {
+            get { return col_07; }
+            set { col_07 = value; }
+        }
+
+        public string Col_06
+        {
+            get { return col_06; }
+            set { col_06 = value; }
+        }
+
+        public string Col_05
+        {
+            get { return col_05; }
+            set { col_05 = value; }
+        }
+
+        public decimal Col_04
+        {
+            get { return col_04; }
+            set { col_04 = value; }
+        }
+
+        public decimal Col_03
+        {
+            get { return col_03; }
+            set { col_03 = value; }
+        }
+
+        public string Col_02
+        {
+            get { return col_02; }
+            set { col_02 = value; }
+        }
+
+        public string Col_01
+        {
+            get { return col_01; }
+            set { col_01 = value; }
+        }
+    }
+}
--- PyramidAccounting/View/Pages/TwoTabControl/Page_Two_报表管理.xaml.cs
+++ PyramidAccounting/View/Pages/TwoTabControl/Page_Two_报表管理.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PA.Model.DataGrid;
 
 namespace PA.View.Pages.TwoTabControl
 {
@@ -22,6 +23,21 @@ namespace PA.View.Pages.TwoTabControl
         public Page_Two_报表管理()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.DataGrid_资产负债表.ItemsSource = null;
+            this.DataGrid_资产负债表.ItemsSource = test();
+        }
+        private List<Model_资产负债表> test()
+        {
+            List<Model_资产负债表> list = new List<Model_资产负债表>();
+            Model_资产负债表 m = new Model_资产负债表();
+            m.Col_02 = "一、资产类";
+            m.Col_06 = "二、负债类";
+            list.Add(m);
+            return list;
         }
     }
 }
