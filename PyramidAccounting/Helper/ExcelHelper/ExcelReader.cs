45f5abcbbca4de92102dcfd4cf6cc2152bc9c4ab
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-13 16:40:58 +0800

    添加excel操作类

--- PyramidAccounting/Helper/ExcelHelper/ExcelReader.cs
+++ PyramidAccounting/Helper/ExcelHelper/ExcelReader.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using xls = Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 
 namespace PA.Helper.ExcelHelper
 {
     class ExcelReader
     {
+        xls.Application xlApp;
+        xls.Workbook xlWorkBook;
+        xls.Worksheet xlWorkSheet;
+        object misValue = System.Reflection.Missing.Value;
+
+        public DataSet ExcelDataSource(string filepath, string sheetname)
+        {
+            string strConn;
+            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filepath + ";Extended Properties=Excel 8.0;";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            OleDbDataAdapter oada = new OleDbDataAdapter("select * from [" + sheetname + "$]", strConn);
+            DataSet ds = new DataSet();
+            oada.Fill(ds);
+            return ds;
+        }
     }
 }
--- PyramidAccounting/Helper/ExcelHelper/ExcelWriter.cs
+++ PyramidAccounting/Helper/ExcelHelper/ExcelWriter.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using xls = Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 
 namespace PA.Helper.ExcelHelper
 {
     class ExcelWriter
     {
+        xls.Application xlApp;
+        xls.Workbook xlWorkBook;
+        xls.Worksheet xlWorkSheet;
+        object misValue = System.Reflection.Missing.Value;
+
+
     }
 }
