d358c730d6f43c1fff8d74c4f72ce103eaa378ea
lijianbin <361931469@qq.com>
2014-03-17 14:35:07 +0800

    用户添加页面设计

---
+++ PyramidAccounting/View/Pages/Pop/系统管理/Page_添加用户.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PA.View.Pages.Pop.系统管理
+{
+    /// <summary>
+    /// Interaction logic for Page_添加用户.xaml
+    /// </summary>
+    public partial class Page_添加用户 : Page
+    {
+        public Page_添加用户()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_PopCommit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_PopClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
