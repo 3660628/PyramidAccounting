8f8305cecbcbc097b917bbe928b23481aa3ab4a5
lijianbin <361931469@qq.com>
2014-03-17 11:18:04 +0800

    页面画一半

---
+++ PyramidAccounting/View/Pages/Page_登录页面.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PA.View.Pages
+{
+    /// <summary>
+    /// Interaction logic for Page_登录页面.xaml
+    /// </summary>
+    public partial class Page_登录页面 : Page
+    {
+        public Page_登录页面()
+        {
+            InitializeComponent();
+        }
+    }
+}
