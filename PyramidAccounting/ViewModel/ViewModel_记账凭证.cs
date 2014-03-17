8b866db391a1933ed81e58c2bc84f45460f17243
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-17 18:01:16 +0800

    录入凭证只保存非空
    VoucherDetail.摘要 != null && VoucherDetail.科目编号 != null

--- PyramidAccounting/ViewModel/ViewModel_记账凭证.cs
+++ PyramidAccounting/ViewModel/ViewModel_记账凭证.cs
@@ -19,7 +19,15 @@ namespace PA.ViewModel
             List<Model_凭证单> Vouchers = new List<Model_凭证单>();
             Vouchers.Add(Voucher);
             new PA.Helper.DataBase.DataBase().InsertPackage(DBTablesName.T_VOUCHER, Vouchers.OfType<object>().ToList());
-            new PA.Helper.DataBase.DataBase().InsertPackage(DBTablesName.T_VOUCHER_DETAIL, VoucherDetails.OfType<object>().ToList());
+            List<Model_凭证明细> NewVoucherDetails = new List<Model_凭证明细>();
+            foreach (Model_凭证明细 VoucherDetail in VoucherDetails)
+            {
+                if (VoucherDetail.摘要 != "" && VoucherDetail.摘要 != null && VoucherDetail.科目编号 != null)
+                {
+                    NewVoucherDetails.Add(VoucherDetail);
+                }
+            }
+            new PA.Helper.DataBase.DataBase().InsertPackage(DBTablesName.T_VOUCHER_DETAIL, NewVoucherDetails.OfType<object>().ToList());
         }
 
         public void UpdateData()
