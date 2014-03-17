7167fb4a400b11de6182a96a72914f0c5aea2f66
lijianbin <361931469@qq.com>
2014-03-17 21:44:39 +0800

    新建两个页面

---
+++ PyramidAccounting/View/Windows/Win_账套页面.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PA.View.Windows
+{
+    /// <summary>
+    /// Interaction logic for Win_账套页面.xaml
+    /// </summary>
+    public partial class Win_账套页面 : Window
+    {
+        public Win_账套页面()
+        {
+            InitializeComponent();
+        }
+    }
+}
