c7f2f9f2cbba2ff1875c5aba6f1de2bea8c87b51
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-18 13:52:34 +0800

    xml helper

--- PyramidAccounting/Helper/XMLHelper/InitXMLConfig.cs
+++ PyramidAccounting/Helper/XMLHelper/InitXMLConfig.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using System.IO;
 
 namespace PA.Helper.XMLHelper
 {
-    class InitXMLConfig
+    public class InitXMLConfig
     {
+        XmlDocument xmldoc;
+
+        public InitXMLConfig()
+        {
+            xmldoc = new XmlDocument();
+        }
+
+        public void InitXML()
+        {
+            XmlDeclaration xmldecl = xmldoc.CreateXmlDeclaration("1.0", "utf-8", null);
+            xmldoc.AppendChild(xmldecl);
+
+            XmlElement xmlelem = xmldoc.CreateElement("", "root", "");
+            xmldoc.AppendChild(xmlelem);
+
+            XmlNode root = xmldoc.SelectSingleNode("root");
+
+            #region 
+            XmlElement 系统信息 = xmldoc.CreateElement("系统信息");
+            XmlElement 帐套信息 = xmldoc.CreateElement("帐套信息");
+            帐套信息.InnerText = "新建帐套";
+            XmlElement 数据库 = xmldoc.CreateElement("数据库");
+            数据库.InnerText = "PyramidAccounting.db";
+
+            系统信息.AppendChild(帐套信息);
+            系统信息.AppendChild(数据库);
+
+            root.AppendChild(系统信息);
+            #endregion
+
+            xmldoc.Save(AppDomain.CurrentDomain.BaseDirectory + "Data\\config.xml");
+        }
     }
 }
