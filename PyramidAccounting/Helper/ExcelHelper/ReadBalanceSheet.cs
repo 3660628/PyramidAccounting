98e8122a99a8991a53842ea7031974fddfd693b5
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-19 18:13:28 +0800

    读excel

--- PyramidAccounting/Helper/ExcelHelper/ReadBalanceSheet.cs
+++ PyramidAccounting/Helper/ExcelHelper/ReadBalanceSheet.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PA.Model.Others;
+using xls = Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 
 namespace PA.Helper.ExcelHelper
 {
@@ -11,13 +15,43 @@ namespace PA.Helper.ExcelHelper
     /// </summary>
     class ReadBalanceSheet
     {
-        public List<Model_BalanceSheet> Read()
+        public List<Model_BalanceSheet> Read(string filepath, string sheetname)
         {
-            List<Model_BalanceSheet> BalanceSheetData = new List<Model_BalanceSheet>();
+            //debug
+            filepath = AppDomain.CurrentDomain.BaseDirectory + "Data\\资产负债表\\资产负债表-行政.xls";
+            sheetname = "1997";
 
+            List<Model_BalanceSheet> BalanceSheetDatas = new List<Model_BalanceSheet>();
+            Model_BalanceSheet BalanceSheetData = new Model_BalanceSheet();
+            string Type = "0";
 
-
-            return BalanceSheetData;
+            string strConn;
+            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filepath + ";Extended Properties=Excel 8.0;";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            OleDbDataAdapter oada = new OleDbDataAdapter("select * from [" + sheetname + "$]", strConn);
+            DataSet ds = new DataSet();
+            oada.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            foreach (DataRow dr in dt.Rows)
+            {
+                BalanceSheetData = new Model_BalanceSheet();
+                {
+                    if (dr[0].ToString().Length <= 2)
+                    {
+                        Type = dr[0].ToString();
+                    }
+                    else
+                    {
+                        BalanceSheetData.Number = int.Parse(dr[0].ToString());
+                        BalanceSheetData.Name = dr[1].ToString();
+                        BalanceSheetData.Type = int.Parse(Type);
+                        BalanceSheetData.DepartmentType = (Type == "1" || Type == "5") ? 1 : 2;
+                        BalanceSheetDatas.Add(BalanceSheetData);
+                    }
+                }
+                
+            }
+            return BalanceSheetDatas;
         }
     }
 }
--- PyramidAccounting/Model/Others/Model_BalanceSheet.cs
+++ PyramidAccounting/Model/Others/Model_BalanceSheet.cs
@@ -8,14 +8,38 @@ namespace PA.Model.Others
     class Model_BalanceSheet
     {
         private int number;
+        private string name;
+        private int departmentType;
+        private int type;
 
+
+        /// <summary>
+        /// 部门编号(资产部,负债部)
+        /// </summary>
+        public int DepartmentType
+        {
+            get { return departmentType; }
+            set { departmentType = value; }
+        }
+        /// <summary>
+        /// 类型编号12345
+        /// </summary>
+        public int Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        /// <summary>
+        /// 编号
+        /// </summary>
         public int Number
         {
             get { return number; }
             set { number = value; }
         }
-        private string name;
-
+        /// <summary>
+        /// 名称
+        /// </summary>
         public string Name
         {
             get { return name; }
