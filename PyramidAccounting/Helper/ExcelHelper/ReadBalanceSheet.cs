2a1cf6291329c808a862038059eef80892454306
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-19 17:33:22 +0800

    加model class

--- PyramidAccounting/Helper/ExcelHelper/ReadBalanceSheet.cs
+++ PyramidAccounting/Helper/ExcelHelper/ReadBalanceSheet.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PA.Model.Others;
 
 namespace PA.Helper.ExcelHelper
 {
+    /// <summary>
+    /// 读资产负债表
+    /// </summary>
     class ReadBalanceSheet
     {
+        public List<Model_BalanceSheet> Read()
+        {
+            List<Model_BalanceSheet> BalanceSheetData = new List<Model_BalanceSheet>();
 
+
+
+            return BalanceSheetData;
+        }
     }
 }
---
+++ PyramidAccounting/Model/Others/Model_BalanceSheet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PA.Model.Others
+{
+    class Model_BalanceSheet
+    {
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
