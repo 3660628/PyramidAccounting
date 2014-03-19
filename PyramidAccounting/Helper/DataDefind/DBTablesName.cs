fcd28cf66565127748af607b0c189a1cf070a813
Huang Shuhang <yhnbgfd@gmail.com>
2014-03-20 03:42:23 +0800

    阿斯顿

--- PyramidAccounting/Helper/DataDefind/DBTablesName.cs
+++ PyramidAccounting/Helper/DataDefind/DBTablesName.cs
@@ -8,12 +8,12 @@ namespace PA.Helper.DataDefind
     class DBTablesName
     {
         private static readonly string t_BOOKS = "T_BOOKS";
-        private static readonly string t_VOUCHER = "T_VOUCHER_";
-        private static readonly string t_VOUCHER_DETAIL = "T_VOUCHER_DETAIL_";
-        private static readonly string t_SUBJECT = "T_SUBJECT_";
+        private static readonly string t_VOUCHER = "T_VOUCHER";
+        private static readonly string t_VOUCHER_DETAIL = "T_VOUCHER_DETAIL";
+        private static readonly string t_SUBJECT = "T_SUBJECT";
         private static readonly string t_SUBJECT_TYPE = "T_SUBJECT_TYPE";
         private static readonly string t_USER = "T_USER";
-        private static readonly string t_RECORD = "T_RECORD_";
+        private static readonly string t_RECORD = "T_RECORD";
         private static readonly string t_YEAR_FEE = "T_YEAR_FEE";
 
 
@@ -31,17 +31,17 @@ namespace PA.Helper.DataDefind
 
         public static string T_VOUCHER
         {
-            get { return DBTablesName.t_VOUCHER + CommonInfo.账薄号; }
+            get { return DBTablesName.t_VOUCHER + "_" + CommonInfo.账薄号; }
         }
 
         public static string T_VOUCHER_DETAIL
         {
-            get { return DBTablesName.t_VOUCHER_DETAIL + CommonInfo.账薄号; }
+            get { return DBTablesName.t_VOUCHER_DETAIL + "_" + CommonInfo.账薄号; }
         }
 
         public static string T_SUBJECT
         {
-            get { return DBTablesName.t_SUBJECT + CommonInfo.制度索引; }
+            get { return DBTablesName.t_SUBJECT + "_" + CommonInfo.制度索引; }
         }
 
         public static string T_SUBJECT_TYPE
@@ -56,7 +56,7 @@ namespace PA.Helper.DataDefind
 
         public static string T_RECORD
         {
-            get { return DBTablesName.t_RECORD + CommonInfo.账薄号; }
+            get { return DBTablesName.t_RECORD + "_" + CommonInfo.账薄号; }
         } 
         #endregion
     }
