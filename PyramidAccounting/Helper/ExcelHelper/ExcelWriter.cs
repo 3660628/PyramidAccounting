db1f64877b11123900443d080b3e78cfdab590e1
Huangshuhang <yhnbgfd@gmail.com>
2014-03-26 17:42:50 +0800

    开始做凭证打印

--- PyramidAccounting/Helper/ExcelHelper/ExcelWriter.cs
+++ PyramidAccounting/Helper/ExcelHelper/ExcelWriter.cs
@@ -9,68 +9,60 @@ using System.IO;
 
 namespace PA.Helper.ExcelHelper
 {
-    class ExcelWriter
+    public class ExcelWriter
     {
+        private string Path = AppDomain.CurrentDomain.BaseDirectory;
         xls.Application xlApp;
         xls.Workbook xlWorkBook;
         xls.Worksheet xlWorkSheet;
         object misValue = System.Reflection.Missing.Value;
 
-
-        /*
-
-        public void ExportExcel(DataSet ds, Pyramid.src.Model.Model_加工单 加工单)
+        public ExcelWriter()
         {
-            File.Copy(AppDomain.CurrentDomain.BaseDirectory + "Data\\正艺纸塑工艺厂加工单.xls", AppDomain.CurrentDomain.BaseDirectory + "正艺纸塑工艺厂加工单.xls", true);
-            xlWorkBook = xlApp.Workbooks.Open(AppDomain.CurrentDomain.BaseDirectory + "正艺纸塑工艺厂加工单.xls");
-            xlWorkSheet = (xls.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            int x = 1, y = 1;
-            foreach (DataRow dr in ds.Tables[0].Rows)
+            try
             {
-                foreach (DataColumn dc in ds.Tables[0].Columns)
-                {
-                    if (dr[dc] != null && dr[dc].ToString() != "")
-                    {
-                        if (dr[dc].ToString().StartsWith("$", false, null))
-                        {
-                            string key = dr[dc].ToString().Replace("$", "");
-                            System.Reflection.PropertyInfo[] properties = 加工单.GetType().GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public);
-                            foreach (System.Reflection.PropertyInfo item in properties)
-                            {
-                                if (item.PropertyType.Name.StartsWith("String"))
-                                {
-                                    if (item.Name == key)
-                                    {
-                                        xlWorkSheet.Cells[y + 1, x] = item.GetValue(加工单, null);
-                                    }
-                                }
-                                else if (item.PropertyType.Name.StartsWith("Boolean"))
-                                {
-                                    if (item.Name == key)
-                                    {
-                                        if (item.GetValue(加工单, null).ToString() == "True")
-                                        {
-                                            xlWorkSheet.Cells[y + 1, x] = "√";
-                                        }
-                                        else
-                                        {
-                                            xlWorkSheet.Cells[y + 1, x] = "";
-                                        }
-                                    }
-                                }
-                            }
-                            //xlWorkSheet.Cells[y + 1, x] = "";
-                        }
-                    }
-                    x++;
-                }
-                y++;
-                x = 1;
+                xlApp = new xls.Application();
             }
-            xlApp.Visible = true;
+            catch (Exception)
+            {
+                Console.WriteLine("想不到EXCEL");
+            }
         }
 
-        */
+        /// <summary>
+        /// 记账凭证
+        /// </summary>
+        public void ExportVouchers()
+        {
+            string SourceXls = Path + @"Data\打印\记账凭证.xls";
+            string ExportXls = Path + @"Data\打印\记账凭证export.xls";
+            File.Copy(SourceXls, ExportXls, true);
+            xlWorkBook = xlApp.Workbooks.Open(ExportXls);
+            xlWorkSheet = (xls.Worksheet)xlWorkBook.Worksheets.get_Item(1);
 
+
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                Console.WriteLine("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
     }
 }
