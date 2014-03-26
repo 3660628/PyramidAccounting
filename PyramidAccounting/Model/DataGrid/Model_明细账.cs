fe329293aff5a91c7569ea1ed97e4adbb189c6ee
lijianbin <361931469@qq.com>
2014-03-26 20:03:34 +0800

    新增明细账模型

---
+++ PyramidAccounting/Model/DataGrid/Model_明细账.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PA.Model.DataGrid
+{
+    class Model_明细账
+    {
+        private string month;
+
+        public string 月
+        {
+            get { return month; }
+            set { month = value; }
+        }
+        private string day;
+
+        public string 日
+        {
+            get { return day; }
+            set { day = value; }
+        }
+        private string comments;
+
+        public string 摘要
+        {
+            get { return comments; }
+            set { comments = value; }
+        }
+        private string day_page;
+
+        public string 日页
+        {
+            get { return day_page; }
+            set { day_page = value; }
+        }
+        private string debt;
+
+        public string 借方金额
+        {
+            get { return debt; }
+            set { debt = value; }
+        }
+        private string credit;
+
+        public string 贷方金额
+        {
+            get { return credit; }
+            set { credit = value; }
+        }
+        private string mark;
+
+        public string 借或贷
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+        private string excess;
+
+        public string 余额
+        {
+            get { return excess; }
+            set { excess = value; }
+        }
+    }
+}
