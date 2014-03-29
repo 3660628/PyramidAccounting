dc49edd8e8837fdf5e5fbc19a0b808726edf2773
lijianbin <361931469@qq.com>
2014-03-29 11:10:35 +0800

    补充凭证管理页面数据加载

--- PyramidAccounting/View/Pages/TwoTabControl/Page_Two_凭证管理.xaml.cs
+++ PyramidAccounting/View/Pages/TwoTabControl/Page_Two_凭证管理.xaml.cs
@@ -45,8 +45,9 @@ namespace PA.View.Pages.TwoTabControl
             this.ComboBox_Date.SelectedIndex = CommonInfo.当前期;
             this.ComboBox_Review.ItemsSource = cbc.GetComboBox_审核();
             this.ComboBox_Review.SelectedIndex = 0;
-            Label_账套名称.Content += "：" + xr.ReadXML("帐套信息");
+            Label_账套名称.Content += "：" + xr.ReadXML("账套信息");
             Label_操作员.Content += "：" + CommonInfo.用户权限 + "\t" +CommonInfo.真实姓名;
+            Label_当前期数.Content += "：" + ComboBox_Date.SelectedValue.ToString();
         }
         private void ReflashData()
         {
