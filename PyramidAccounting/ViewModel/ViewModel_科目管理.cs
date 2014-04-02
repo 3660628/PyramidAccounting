03db55a69bba93b333da855b378158461322313e
lijianbin <361931469@qq.com>
2014-04-02 10:08:58 +0800

    优化代码

--- PyramidAccounting/ViewModel/ViewModel_科目管理.cs
+++ PyramidAccounting/ViewModel/ViewModel_科目管理.cs
@@ -28,7 +28,10 @@ namespace PA.ViewModel
         }
         public List<Model_科目管理> GetSujectData(int type)
         {
-            string sql = "select a.fee,b.* from " + DBTablesName.T_SUBJECT + " b left join t_yearfee a on a.bookid='" + CommonInfo.账薄号 + "' and a.subject_id=b.subject_id where  b.subject_type=" + type + " order by b.id,b.used_mark";
+            string sql = "select a.fee,b.* from " + DBTablesName.T_SUBJECT 
+                + " b left join t_yearfee a on a.bookid='" + CommonInfo.账薄号 
+                + "' and a.subject_id=b.subject_id where  b.subject_type=" + type 
+                + " order by b.id,b.used_mark";
             DataTable dt = db.Query(sql).Tables[0];
             List<Model_科目管理> list = new List<Model_科目管理>();
             for (int i = 0; i < dt.Rows.Count; i++)
@@ -51,7 +54,8 @@ namespace PA.ViewModel
         }
         public List<Model_科目管理> GetChildSubjectData(string parent_id)
         {
-            string sql = "select a.*,b.fee from " + DBTablesName.T_SUBJECT + " a left join t_yearfee b on a.subject_id=b.subject_id  where b.bookid='"
+            string sql = "select a.*,b.fee from " + DBTablesName.T_SUBJECT 
+                + " a left join t_yearfee b on a.subject_id=b.subject_id  where b.bookid='"
                 + CommonInfo.账薄号 + "' and a.parent_id='" + parent_id + "' order by a.id";
             DataTable dt = db.Query(sql).Tables[0];
             List<Model_科目管理> list = new List<Model_科目管理>();
@@ -70,24 +74,30 @@ namespace PA.ViewModel
        
         public void UpdateUsedMark(Model_科目管理 m)
         {
-            string sql = "update " + DBTablesName.T_SUBJECT + " set used_mark=" + m.Used_mark + " where id=" + m.ID;
+            string sql = "update " + DBTablesName.T_SUBJECT + " set used_mark=" 
+                + m.Used_mark + " where id=" + m.ID;
             db.Excute(sql);
         }
 
         public void UpdateBorrowMark(Model_科目管理 m)
         {
-            string sql = "update " + DBTablesName.T_SUBJECT + " set Borrow_Mark=" + ((m.借贷标记)?1:-1) + " where id=" + m.ID;
+            string sql = "update " + DBTablesName.T_SUBJECT + " set Borrow_Mark=" 
+                + ((m.借贷标记)?1:-1) + " where id=" + m.ID;
             db.Excute(sql);
         }
 
         public void UpdateChildSubject(Model_科目管理 m)
         {
             List<string> sqlList = new List<string>();
-            string sql1 = "update " + DBTablesName.T_SUBJECT + " set subject_id='" + m.科目编号 + "',subject_name='" + m.科目名称 + "' where id=" + m.ID;
+            string sql1 = "update " + DBTablesName.T_SUBJECT + " set subject_id='" 
+                + m.科目编号 + "',subject_name='" + m.科目名称 + "' where id=" + m.ID;
             sqlList.Add(sql1);
-            string sql2 = "update " + DBTablesName.T_YEAR_FEE + " set fee='" + m.年初金额 + "',subject_id='" + m.科目编号 +"' where parentid='" + m.父ID + "' and subject_id='" + m.科目编号 + "' and bookid='" + CommonInfo.账薄号 + "'";
+            string sql2 = "update " + DBTablesName.T_YEAR_FEE + " set fee='" 
+                + m.年初金额 + "',subject_id='" + m.科目编号 +"' where parentid='" 
+                + m.父ID + "' and subject_id='" + m.科目编号 + "' and bookid='" + CommonInfo.账薄号 + "'";
             sqlList.Add(sql2);
-            string sql3 = "update T_YEARFEE set fee = (select total(fee) from T_YEARFEE where parentid=" + m.父ID + ") where subject_id=" + m.父ID;
+            string sql3 = "update T_YEARFEE set fee = (select total(fee) from T_YEARFEE where parentid=" 
+                + m.父ID + ") where subject_id=" + m.父ID;
             sqlList.Add(sql3);
             db.BatchOperate(sqlList);
         }
@@ -99,11 +109,13 @@ namespace PA.ViewModel
             List<string> sqlList = new List<string>();
             foreach(Model_科目管理 m in list)
             {
-                string sql = "insert into t_yearfee values ('" + m.科目编号 + "','" + m.年初金额 + "','" + m.父ID + "','" + CommonInfo.账薄号 + "')";
+                string sql = "insert into t_yearfee values ('" + m.科目编号 + "','" 
+                    + m.年初金额 + "','" + m.父ID + "','" + CommonInfo.账薄号 + "')";
                 sqlList.Add(sql);
                 parentid = m.父ID;
             }
-            string sql3 = "update T_YEARFEE set fee = (select total(fee) from T_YEARFEE where parentid=" + parentid + ") where subject_id=" + parentid;
+            string sql3 = "update T_YEARFEE set fee = (select total(fee) from T_YEARFEE where parentid=" 
+                + parentid + ") where subject_id=" + parentid;
             sqlList.Add(sql3);
             db.BatchOperate(sqlList);
         }
@@ -116,18 +128,21 @@ namespace PA.ViewModel
             {
                 string sql = "delete from " + DBTablesName.T_SUBJECT + " where id=" + m.ID;
                 sqlList.Add(sql);
-                sql = "delete from " + DBTablesName.T_YEAR_FEE + " where subject_id='" + m.科目编号 + "' and parentid='" + m.父ID + "' and bookid='" + CommonInfo.账薄号 + "'";
+                sql = "delete from " + DBTablesName.T_YEAR_FEE + " where subject_id='" 
+                    + m.科目编号 + "' and parentid='" + m.父ID + "' and bookid='" + CommonInfo.账薄号 + "'";
                 sqlList.Add(sql);
                 parentid = m.父ID;
             }
-            string sql3 = "update T_YEARFEE set fee = (select total(fee) from T_YEARFEE where parentid=" + parentid + ") where subject_id=" + parentid;
+            string sql3 = "update T_YEARFEE set fee = (select total(fee) from T_YEARFEE where parentid=" 
+                + parentid + ") where subject_id=" + parentid;
             sqlList.Add(sql3);
             db.BatchOperate(sqlList);
         }
 
         public string GetSubjectID(string name)
         {
-            string sql = "select subject_id from " + DBTablesName.T_SUBJECT + " where subject_name='" + name + "' and parent_id = 0";
+            string sql = "select subject_id from " + DBTablesName.T_SUBJECT + " where subject_name='" 
+                + name + "' and parent_id = 0";
             return db.GetAllData(sql).Split('\t')[0].Split(',')[0];
         }
     }
