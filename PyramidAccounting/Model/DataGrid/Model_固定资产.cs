8ff17630b6d93f519791beb9f0ba8a84a84a7ff5
henryWhong <hernthay@hotmail.com>
2014-04-07 00:55:22 +0800

    固定资产的model，固定资产的View_Model开始写

--- PyramidAccounting/Model/DataGrid/Model_固定资产.cs
+++ PyramidAccounting/Model/DataGrid/Model_固定资产.cs
@@ -7,5 +7,89 @@ namespace PA.Model.DataGrid
 {
     class Model_固定资产
     {
+        private int rowid;
+        private string sourceName;
+        private string unit;
+        private string amount;
+        private string price;
+        private int usedYear;
+        private string buyDate;
+        private string deparment;
+        private string createDate;
+        private string voucherNo;
+        private string comments;
+        private int deleteMark;
+
+        public int 序号
+        {
+            get { return rowid; }
+            set { rowid = value; }
+        }
+
+        public string 名称及规格
+        {
+            get { return sourceName; }
+            set { sourceName = value; }
+        }
+
+        public string 单位
+        {
+            get { return unit; }
+            set { unit = value; }
+        }
+
+        public string 数量
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public string 价格
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int 使用年限
+        {
+            get { return usedYear; }
+            set { usedYear = value; }
+        }
+
+        public string 购置日期
+        {
+            get { return buyDate; }
+            set { buyDate = value; }
+        }
+
+        public string 使用部门
+        {
+            get { return deparment; }
+            set { deparment = value; }
+        }
+
+        public string 报废日期
+        {
+            get { return createDate; }
+            set { createDate = value; }
+        }
+
+        public string 凭证编号
+        {
+            get { return voucherNo; }
+            set { voucherNo = value; }
+        }
+
+        public string 备注
+        {
+            get { return comments; }
+            set { comments = value; }
+        }
+
+        public int 删除标志
+        {
+            get { return deleteMark; }
+            set { deleteMark = value; }
+        }
     }
 }
--- PyramidAccounting/ViewModel/ViewModel_固定资产.cs
+++ PyramidAccounting/ViewModel/ViewModel_固定资产.cs
@@ -2,11 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PA.Helper.DataBase;
+using PA.Model.DataGrid;
+using PA.Helper.DataDefind;
+using System.Data;
 
 namespace PA.ViewModel
 {
     class ViewModel_固定资产
     {
+        DataBase db = new DataBase();
+        private string sql = string.Empty;
 
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <returns></returns>
+        public List<Model_固定资产> GetAllSource()
+        {
+            sql = "";
+            List<Model_固定资产> list = new List<Model_固定资产>();
+            DataSet ds = db.Query(sql);
+            if (ds != null)
+            {
+                DataTable dt = ds.Tables[0];
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    Model_固定资产 m = new Model_固定资产();
+                    DataRow d = dt.Rows[i];
+                    m.序号 = i + 1;
+                    m.名称及规格 = d[1].ToString();
+                    m.单位 = d[2].ToString();
+                    m.数量 = d[3].ToString();
+                    m.价格 = d[4].ToString();
+                    m.使用年限 = Convert.ToInt32(d[5]);
+                    m.购置日期 = Convert.ToDateTime(d[6].ToString()).ToString("yyyy-MM-dd HH:mm:ss");
+                    m.使用部门 = d[7].ToString();
+                    m.报废日期 = Convert.ToDateTime(d[8].ToString()).ToString("yyyy-MM-dd HH:mm:ss");
+                    m.凭证编号 = d[9].ToString();
+                    m.凭证编号 = d[10].ToString();
+                    list.Add(m);
+                }
+            }
+            return list;
+        }
     }
 }
