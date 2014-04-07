c1c1eafad49e331a0a83bae955c3aac3e792dd3b
lijianbin <361931469@qq.com>
2014-04-07 15:28:36 +0800

    fix bug

--- PyramidAccounting/ViewModel/ViewModel_ReportManager.cs
+++ PyramidAccounting/ViewModel/ViewModel_ReportManager.cs
@@ -58,56 +58,30 @@ namespace PA.ViewModel
         }
         public List<Model_报表类> GetAdministrativeExpenseDetail(int index)
         {
+            List<string> sqlList = new List<string>();
             List<Model_报表类> list = new List<Model_报表类>();
-            string sql = "SELECT a.SUBJECT_ID,a.fee,b.fee FROM (SELECT SUBJECT_ID,fee FROM " +
-                DBTablesName.T_FEE + " WHERE PERIOD = " + index + ") a LEFT JOIN (SELECT SUBJECT_ID,total(fee) AS fee FROM "
-                + DBTablesName.T_FEE + " WHERE PERIOD <= " + index 
-                +" GROUP BY	SUBJECT_ID	) b ON a.SUBJECT_ID = b.SUBJECT_ID "
-                + "WHERE a.SUBJECT_ID IN ('401','404','407','501','502','505','303') ";
-            /* --创建临时表
- create temporary table sbtemp(detail text,period int,subject_id text,fee decimal);
- --插入临时表
- insert into sbtemp
- SELECT
-             a.DETAIL,
-             a.PERIOD,
-             b.SUBJECT_ID,
-             a.fee
-         FROM
-             (
-                 SELECT
-                     a.DETAIL,
-                     b.PERIOD,
-                     total(a.DEBIT - a.CREDIT) AS fee
-                 FROM
-                     T_VOUCHERDETAIL_20140406154717 a
-                 LEFT JOIN T_VOUCHER_20140406154717 b ON a.PARENTID = b.ID
-                 GROUP BY
-                     a.DETAIL,
-                     b.PERIOD
-             ) a
-         LEFT JOIN T_SUBJECT_0 b ON a.DETAIL = b.SUBJECT_NAME
-         WHERE
-             b.PARENT_ID LIKE '501%' 
-             order by b.subject_id
- ;
- --计算行政费用支出明细表
- select a.subject_id,a.fee,b.fee from(select subject_id,fee from sbtemp
-  where period=1)
-  a,(select subject_id,sum(fee) as fee from sbtemp where period<=1 group by subject_id) b
-   where a.subject_id=b.subject_id 
-   and a.subject_id not in (select subject_id from t_subject_0 where parent_id='501') 
-   order by a.subject_id
-       */
-            DataTable dt = db.Query(sql).Tables[0];
-            foreach (DataRow d in dt.Rows)
-            {
-                Model_报表类 m = new Model_报表类();
-                m.年初数 = d[2].ToString();
-                m.期末数 = d[1].ToString();
-                list.Add(m);
-            }
+            string _sql1 = "create temporary table sbtemp(detail text,period int,subject_id text,fee decimal)";
+            sqlList.Add(_sql1);
+            string _sql2 = "insert into sbtemp SELECT a.DETAIL,b.PERIOD,total(a.DEBIT - a.CREDIT) AS fee FROM "
+                    + DBTablesName.T_VOUCHER_DETAIL + " a LEFT JOIN "
+                    + DBTablesName.T_VOUCHER + " b ON a.PARENTID = b.ID GROUP BY a.DETAIL,b.PERIOD  ";
+            /*--计算行政费用支出明细表
+            select a.subject_id,a.fee,b.fee from(select subject_id,fee from sbtemp
+             where period=1)
+             a,(select subject_id,sum(fee) as fee from sbtemp where period<=1 group by subject_id) b
+              where a.subject_id=b.subject_id 
+              and a.subject_id not in (select subject_id from t_subject_0 where parent_id='501') 
+              order by a.subject_id
+                       DataTable dt = db.Query(sql).Tables[0];
+                       foreach (DataRow d in dt.Rows)
+                       {
+                           Model_报表类 m = new Model_报表类();
+                           m.年初数 = d[2].ToString();
+                           m.期末数 = d[1].ToString();
+                           list.Add(m);
+                       }*/
             return list;
+       
         }
     }
 }
