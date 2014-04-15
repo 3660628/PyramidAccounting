ae31106f971ab6578f07904f34892c06e9d301c6
Huangshuhang <yhnbgfd@gmail.com>
2014-04-15 16:27:28 +0800

    修改变量作用域

--- PyramidAccounting/ViewModel/ViewModel_凭证管理.cs
+++ PyramidAccounting/ViewModel/ViewModel_凭证管理.cs
@@ -12,7 +12,6 @@ namespace PA.ViewModel
     class ViewModel_凭证管理
     {
         private string DateFormat = "yyyy-MM-dd";
-        private Guid  LastID = Guid.Empty;
         private Model_凭证管理 LastData = new Model_凭证管理();
         private DataBase db = new DataBase();
         /// <summary>
@@ -23,6 +22,7 @@ namespace PA.ViewModel
         public List<Model_凭证管理> GetData(string whereParm)
         {
             List<Model_凭证管理> datas = new List<Model_凭证管理>();
+            Guid LastID = Guid.Empty;
             string sql = "SELECT "
                             + "voucher.ID,"
                             + "detail.VOUCHER_NO,"
